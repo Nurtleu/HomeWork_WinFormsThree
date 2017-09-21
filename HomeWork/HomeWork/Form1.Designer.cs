c3b782b67bc29f62f2eb30d9a8ca31b145d00a7f
Nurtleu <nur71701@gmail.com>
2017-09-21 16:01:19 +0600

    HW3

---
+++ HomeWork/HomeWork/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Data_textBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxData.Location = new System.Drawing.Point(28, 22);
+            this.textBoxData.MaxLength = 10;
+            this.textBoxData.Multiline = true;
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.Size = new System.Drawing.Size(149, 29);
+            this.textBoxData.TabIndex = 0;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOK.Location = new System.Drawing.Point(28, 66);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(149, 30);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Clear.Location = new System.Drawing.Point(183, 66);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(149, 30);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Data_textBox
+            // 
+            this.Data_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Data_textBox.Location = new System.Drawing.Point(183, 22);
+            this.Data_textBox.MaxLength = 20;
+            this.Data_textBox.Multiline = true;
+            this.Data_textBox.Name = "Data_textBox";
+            this.Data_textBox.Size = new System.Drawing.Size(149, 29);
+            this.Data_textBox.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxData);
+            this.panel1.Controls.Add(this.Clear);
+            this.panel1.Controls.Add(this.Data_textBox);
+            this.panel1.Controls.Add(this.buttonOK);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 164);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 163);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxData;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox Data_textBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ HomeWork/HomeWork/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Data_textBox.Clear();
+            textBoxData.Clear();
+        }
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            DateTime data = new DateTime();
+            data = DateTime.Parse(textBoxData.Text);
+            Data_textBox.Text = data.ToString("dddd");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
